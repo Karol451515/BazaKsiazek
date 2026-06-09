6ca6193a8939f8dd355a84e0ad3675b07ede3684
Karol Kręcioch <karolkrecioch@gmail.com>
2026-06-09 12:49:00 +0200

    Dodano szkielete funckji

---
+++ operacje.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "biblioteka.h"
+
+void wyswietlKsiazki() {
+	printf("Funckja niezaimplementowana");
+}
+
+void dodajKsiazke()
+{
+	printf("Funckja niezaimplementowana");
+}
