502b84d322fc79249b1b7d1169461001459f9e9b
Karol Kręcioch <karolkrecioch@gmail.com>
2026-06-09 18:31:09 +0200

    funkcja zapiszdopliku

--- pliki.c
+++ pliki.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include "biblioteka.h"
 
-void zapiszDoPliku() {
-	printf("Zapis do pliku\n");
+void zapiszDoPliku(Ksiazka baza[], int liczba ) {
+	FILE* plik = fopen(PLIK_BAZY, "wb"); //otwieranie pliku
+
+	if (plik == NULL) {
+		printf("Nie mozna otwrozyc pliku do zapisu.\n");
+		return;
+	}
+
+	fwrite(&liczba, sizeof(int), 1, plik); //zapis liczby książęk
+	fwirte(baza, sizeof(Ksiazka), liczba, plik);
+
+	fclose(plik);
+	printf("Dane zapisano do pliku\n");
 }
 
-void wczytajZPliku() {
+void wczytajZPliku(Ksiazka baza[], int *liczba) {
 	printf("Wczytywanie z pliku\n");
 }
 
-void utworzPrzykladowaBaze() {
+void utworzPrzykladowaBaze(Ksiazka baza[], int* liczba) {
 	printf("Tworzenie przykladowej bazy\n");
 }
